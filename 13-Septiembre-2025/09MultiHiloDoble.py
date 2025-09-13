13e8b9dbc7bf7737bbf5746eb3e012d782704600
Alexis Cortes <ing.cortesrf.94@gmail.com>
2025-09-13 13:17:41 -0600

    Multihilo doble

---
+++ 13-Septiembre-2025/09MultiHiloDoble.py
@@ -0,0 +1,40 @@
+from machine import Pin
+import utime
+import _thread
+
+pinSwMasFrec = Pin(15, Pin.IN, Pin.PULL_DOWN)
+pinSwMenFrec = Pin(16, Pin.IN, Pin.PULL_DOWN)
+
+pinLed1 = Pin(21, Pin.OUT)
+pinLed2 = Pin(22, Pin.OUT)
+
+global intTiempo
+intTiempo = 500
+
+def nucleo1():
+    global intTiempo
+    while True:
+        pinLed1.on()
+        utime.sleep_ms(intTiempo)
+        pinLed1.off()
+        utime.sleep_ms(intTiempo)
+        
+_thread.start_new_thread(nucleo1, ())
+
+#Nucleo principal
+while True:
+    global intTiempo
+    if pinSwMasFrec():
+        utime.sleep_ms(20) #Tiempo de rebote
+        intTiempo -= 20
+        pinLed2.toggle()
+        
+    if pinSwMenFrec():
+        utime.sleep_ms(20)
+        intTiempo += 20
+        pinLed2.toggle()
+    
+    
+    
+    
+    
