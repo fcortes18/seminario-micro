e6b0dd478ed847fd1faee8e849ff210dfe063c78
Alexis Cortes <ing.cortesrf.94@gmail.com>
2025-10-10 19:05:37 -0600

    Numeros testing

---
+++ 10-Octubre-2025/ArchivosDelDrive/Numeros_Testing.m
@@ -0,0 +1,29 @@
+clear all; close all; clc;
+
+load('Pesos_Neurona_Numeros.mat')
+
+img_ent = imread('tres.jpg');
+
+img_ent = im2double(img_ent);
+
+img_gris = rgb2gray(img_ent);
+
+img_gris = imadjust(img_gris, [0.2 0.5]);
+
+img_com = imcomplement(img_gris);
+
+img_test = imresize(img_com, [28 28]);
+
+imshow(img_test, 'InitialMagnification', 800)
+
+size(img_test)
+
+P1 = [img_test(:)];
+
+net = W*P1 + b;
+
+[a win] = max(net);
+
+net'
+
+NumeroReconocido = win - 1;
